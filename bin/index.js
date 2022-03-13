842bdec45891547cd3c1eec86441cd4b63d22627
Richard Albert Leddy <richardaleddy@gmail.com>
2022-03-12 20:24:24 -0800

    start building up cli

--- bin/index.js
+++ bin/index.js
@@ -12,3 +12,10 @@ console.log(process.argv[1])
 
 papa.identify_me()
 
+let output = papa.browser_code()
+for ( let fcontent of output ) {
+    let {file_path, content} = fcontent
+    console.log(file_path)
+    console.log("---------------------------")
+    console.log(content)
+}
